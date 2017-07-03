077886d259b10d0a19f8e4254cf91d0cfc3ed2eb
miya0001 <miya@wpist.me>
2017-07-03 18:58:11 +0900

    first commit

---
+++ wp-resas.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     RESAS API Client for WordPress
+ * Plugin URI:      https://github.com/miya0001/wp-resas
+ * Description:     RESAS API Client for WordPress
+ * Author:          Takayuki Miyauchi
+ * Author URI:      https://firegoby.jp/
+ * Text Domain:     wp-resas
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WP_Resas
+ */
+
+require dirname( __FILE__ ) . '/vendor/autoload.php';
